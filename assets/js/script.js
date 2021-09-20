a7cf26a60b03d625a9b925ea463e20cbe968ef45
Shirly <shirlyd@axioned.com>
2021-09-20 15:56:30 +0530

    js function commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,21 @@
-/* Author: 
+let countDisplay = document.querySelector('.display-count');
+let countMinus = document.querySelector('.count-minus');
+let countAdd = document.querySelector('.count-add');
+let count = 0;
 
-*/
+function numberDisplay() {
+    countDisplay.innerHTML = count;
+}
+
+countMinus.addEventListener("click", () => {
+    count--;
+    numberDisplay();
+})
+
+countAdd.addEventListener("click", () => {
+    count++;
+    numberDisplay();
+})
 
 
 
